2444ccd53dde2e05cbacff8b3c014ab17c150639
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-03-19 10:20:45 +0300

    use the do while loop

---
+++ CMP1201_Conditional_statements/do_while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 0;
+    // do {
+    //     printf("Enter your age: ");
+    //     scanf("%d", &age);
+    // } while (age < 0);
+
+    while(age <0)
+    {
+       printf("Enter your age: ");
+        scanf("%d", &age); 
+    }
+}

c7dcb5a7ae8d5b197c2301ff62f5ef7498cb7fc4
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-03-19 10:21:19 +0300

    code for using the break statement

---
+++ CMP1201_Conditional_statements/break_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+    while(1)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &x);
+        printf("The number is %d\n",x);
+        if (x < 0)
+        {
+            printf("Invalid number\n");
+            break;
+        }
+        
+    }
+    printf("We are out of the loop");
+
+    return 0;
+
+}

c0e29fdbfa05e78b8912832f6531532be1bac319
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-03-21 10:05:59 +0300

    break and continue in loops

---
+++ Loops/tutorial/break_for.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    for (int i=0;i<10;i++)
+    {
+        
+        if ( i == 5)
+        {
+            continue;
+        }
+    printf("%d\n",i);
+        
+    }
+
+
+
+    return 0;
+}
